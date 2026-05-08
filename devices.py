26516896ffb319a494bff17fafb416d152772822
Jiabao Liu <3072721651@qq.com>
2026-05-08 12:55:26 +0800

    add layer 2 function

--- devices.py
+++ devices.py
@@ -1,5 +1,10 @@
-from protocol import split_data, create_ip_packet, create_data_segment
-from config import HOST_A_IP, HOST_B_IP, R1_IF1_IP, R1_IF2_IP
+from protocol import split_data, create_ip_packet, create_data_segment , create_ethernet_frame
+from config import (HOST_A_IP, 
+                    HOST_B_IP, 
+                    R1_IF1_IP, 
+                    R1_IF2_IP, 
+                    R1_IF1_MAC, 
+                    R1_IF2_MAC)
 
 class Host:
     def __init__(self, name, ip, mac):
@@ -7,7 +12,8 @@ class Host:
         self.ip = ip
         self.mac = mac
         self.routing_table = self.create_routing_table()
-
+        self.mac_table = self.create_mac_table()
+        
     def create_routing_table(self):
         if self.ip == HOST_A_IP:
             return {
@@ -20,6 +26,19 @@ class Host:
             }
 
         return {}
+    def create_mac_table(self):
+        if self.ip == HOST_A_IP:
+            return {
+                R1_IF1_IP: R1_IF1_MAC
+            }
+
+        if self.ip == HOST_B_IP:
+            return {
+                R1_IF2_IP: R1_IF2_MAC
+            }
+
+        return {}
+    
 
     def send_application_data(self, dst_ip, message_size):
         data = "A" * message_size
@@ -44,8 +63,8 @@ class Host:
                 f"(DATA, seq={segment.seq}) (encapsulation)"
             )
 
-            packet, next_hop_ip = self.send_segment_to_network_layer(segment, dst_ip)
-            packets.append(packet, next_hop_ip)
+            packet, next_hop_ip, frame = self.send_segment_to_network_layer(segment, dst_ip)
+            packets.append((packet, next_hop_ip, frame))
 
         return packets
 
@@ -60,31 +79,69 @@ class Host:
             f"SRC_IP={packet.src_ip}, DST_IP={packet.dst_ip}, TTL={packet.ttl}"
         )
 
-        next_hop_ip = self.forward_packet_from_network_layer(packet)
+        next_hop_ip, frame = self.forward_packet_from_network_layer(packet)
 
-        return packet, next_hop_ip
+        return packet, next_hop_ip, frame
 
     def forward_packet_from_network_layer(self, packet):
-
         dst_ip = packet.dst_ip
-        print(f"{self.name}: Layer 3: Destination IP read: {packet.dst_ip}")
+
+        print(f"{self.name}: Layer 3: Destination IP read: {dst_ip}")
         print(f"{self.name}: Layer 3: Routing table lookup performed")
 
-        next_hop_ip = self.lookup_next_hop_ip(packet.dst_ip)
+        next_hop_ip = self.lookup_next_hop_ip(dst_ip)
 
         print(f"{self.name}: Layer 3: Next-hop IP determined: {next_hop_ip}")
 
+        outgoing_interface = self.select_outgoing_interface(next_hop_ip)
 
         print(f"{self.name}: Layer 3: Outgoing interface selected")
         print(f"{self.name}: Layer 3: Packet forwarded to Data Link Layer")
 
-        return next_hop_ip
+        frame = self.send_packet_to_data_link_layer(packet, next_hop_ip, outgoing_interface)
+
+        return next_hop_ip, frame
 
     def lookup_next_hop_ip(self, dst_ip):
         if dst_ip in self.routing_table:
             return self.routing_table[dst_ip]
 
         return dst_ip
+    
+    def select_outgoing_interface(self, next_hop_ip):
+        return {
+            "ip": self.ip,
+            "mac": self.mac
+        }
+    def send_packet_to_data_link_layer(self, packet, next_hop_ip, outgoing_interface):
+        print(f"{self.name}: Layer 2: Packet received from Network Layer")
+
+        dst_mac = self.lookup_mac_address(next_hop_ip)
+
+        print(
+            f"{self.name}: Layer 2: Destination MAC lookup for next-hop IP "
+            f"({next_hop_ip}) \u2192 {dst_mac}"
+        )
+
+        frame = create_ethernet_frame(
+            src_mac=outgoing_interface["mac"],
+            dst_mac=dst_mac,
+            packet=packet
+        )
+
+        print(
+            f"{self.name}: Layer 2: Frame created: "
+            f"SRC_MAC={frame.src_mac}, DST_MAC={frame.dst_mac}"
+        )
+        print(f"{self.name}: Layer 2: Frame sent")
+
+        return frame
+
+    def lookup_mac_address(self, next_hop_ip):
+        if next_hop_ip in self.mac_table:
+            return self.mac_table[next_hop_ip]
+
+        return None
 class Router:
     def __init__(self, name):
         self.name = name
